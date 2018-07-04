9e8640294d92c8c1a99a54a56a0302e10c46f951
Оксана Ткаченко <oksana@otbook.local>
2018-07-04 09:53:40 +0300

    added map to contacts

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 ymaps.ready(init);
 
 function init(){
-  var myMap = new ymaps.Map("map", {
+  var myMap = new ymaps.Map('map', {
     center: [56.326887, 44.005986],
     zoom: 10
   });
+  var mapContacts = new ymaps.Map('contacts-map', {
+    center: [56.321591, 43.952743],
+    zoom: 13
+  });
+/*
+  var myPlacemark = new ymaps.Placemark([56.32, 43.95], {
+    hintContent: 'Содержимое всплывающей подсказки',
+    balloonContent: 'Содержимое балуна'
+  });
+
+  mapContacts.geoObjects.add(myPlacemark);
+  */
+
 }
 
 
